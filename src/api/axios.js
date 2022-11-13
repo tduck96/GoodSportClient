e070496140e138f6a0442e148291964423316db7
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-11-12 18:50:43 -0600

    media queries for homepage

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'https://goodsportruff.azurewebsites.net/api';
-
+// const BASE_URL = 'https://goodsportruff.azurewebsites.net/api';
+const BASE_URL = 'http://localhost:3002/api';
 export default axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type' : 'application/json',
