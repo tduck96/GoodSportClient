7dbd9f4d2f6843764635af1c61e027ed6f462f9e
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-11-15 07:57:03 -0600

    updates to axios component

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//  const BASE_URL = 'https://goodsportruff.azurewebsites.net/api';
- const BASE_URL = 'http://localhost:3003/api';
+ const BASE_URL = 'https://goodsportruff.azurewebsites.net/api';
+ 
  
 
 export default axios.create({
