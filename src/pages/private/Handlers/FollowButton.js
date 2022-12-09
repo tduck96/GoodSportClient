1d5264ef5d00b3534d15757900beb85dd393affb
Thomas Duckworth <98187423+tduck96@users.noreply.github.com>
2022-12-08 18:06:39 -0600

    added followComponent

---
+++ src/pages/private/Handlers/FollowButton.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from '../../../api/axios';
+import useAuth from '../../../hooks/useAuth';
+import { useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+const FollowButton = () => {
+    const [status, setStatus] = useState(false);
+    const {id} = useParams();
+    const {auth} = useAuth();
+
+    useEffect(() => {
+        getStatus();
+    }, []);
+
+    const getStatus = async () => {
+        try {
+            const result = await axios.get(`/user/isFollowing?userId=${auth.userId}&followingId=${id}`)
+            console.log(result.data);
+            setStatus(result.data);
+        }
+        catch(err)
+        {
+            console.error(err);
+        }
+    }
+
+    const followHandler = async () => {
+        try {
+            const response = await axios.post(`/user/${auth.userId}/follow`, {
+                userProfileId: auth?.userId,
+                followId: id
+            });
+
+            console.log(response.data);
+        }
+        catch(err) {
+            console.error(err);
+        }
+        getStatus();
+    }
+
+    const unfollowHandler = async () => {
+        try {
+            const response = await axios.delete(`/user/unfollow/${id}?userId=${auth?.userId}`)
+            console.log(response);
+        }
+        catch(err) {
+            console.error(err)
+        }
+        getStatus();
+    }
+  return (
+    <div>
+       { 
+       status === true ? 
+         <Button variant = "danger" onClick = {unfollowHandler}>Unfollow</Button>
+       : 
+         <Button variant = "primary" onClick = {followHandler}>Follow</Button>
+        }
+    </div>
+  )
+}
+
+export default FollowButton
